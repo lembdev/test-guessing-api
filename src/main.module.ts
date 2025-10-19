78df79d8f23fe29556e0e5fae9b3337d3c5c7fb2
Alexander <lembdev@gmail.com>
2025-10-19 13:23:13 +0200

    feat: add API module to NestJS DI

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ApiModule } from './api/api.module';
 import * as configs from './infrastructure/configs';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: Object.values(configs) }),
+    ApiModule,
   ],
   controllers: [],
   providers: [],

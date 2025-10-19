0ac1d6fd0b7395137e3b907765ad78f712817c00
Alexander <lembdev@gmail.com>
2025-10-19 14:45:16 +0200

    feat: extend Room service

--- src/app/services/room/room.service.ts
+++ src/app/services/room/room.service.ts
@@ -1,4 +1,10 @@
-import type { IRoom, IRoomUuid } from 'src/contracts';
+import type {
+  IGuess,
+  IPlayer,
+  IPlayerUuid,
+  IRoom,
+  IRoomUuid,
+} from 'src/contracts';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ulid } from 'ulid';
 import { secretNumberGenerator } from '../../helpers';
@@ -29,7 +35,7 @@ export class RoomService {
       uuid: ulid() as IRoomUuid,
       name: name || `Room ${this._rooms.size + 1}`,
       secretNumber: secretNumberGenerator(),
-      players: [],
+      players: new Map(),
     };
 
     this._rooms.set(room.uuid, room);
@@ -39,5 +45,39 @@ export class RoomService {
 
   delete(roomUuid: IRoomUuid) {
     this._rooms.delete(roomUuid);
+  }
+
+  resetSecretNumber(room: IRoom) {
+    room.secretNumber = secretNumberGenerator();
+  }
+
+  resetAllPlayersGuesses(room: IRoom) {
+    room.players.forEach((value, key) => {
+      value.guess = null;
+      room.players.set(key, value);
+    });
+  }
+
+  getAllPlayers(room: IRoom): IPlayerUuid[] {
+    return Array.from(room.players.keys());
+  }
+
+  addPlayerToRoom(room: IRoom, player: IPlayer) {
+    room.players.set(player.uuid, { player, guess: null });
+  }
+
+  removePlayerFromRoom(room: IRoom, player: IPlayer) {
+    room.players.delete(player.uuid);
+  }
+
+  setPlayerGuess(room: IRoom, player: IPlayer, guess: IGuess) {
+    const playerData = room.players.get(player.uuid);
+
+    if (playerData) {
+      playerData.guess = guess;
+      room.players.set(player.uuid, playerData);
+    } else {
+      throw new NotFoundException('Room player not found');
+    }
   }
 }

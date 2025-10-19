eab69f242952099195fe8f93ec4aa042155ba4fc
Alexander <lembdev@gmail.com>
2025-10-19 14:45:59 +0200

    feat: extend Player service

--- src/app/services/player/player.service.ts
+++ src/app/services/player/player.service.ts
@@ -6,22 +6,30 @@ import { ulid } from 'ulid';
 export class PlayerService {
   private _players: Map<ISocketId, IPlayer> = new Map();
 
+  findManyByUuids(uuids: IPlayerUuid[]) {
+    return Array.from(this._players.values()).filter((player) =>
+      uuids.includes(player.uuid),
+    );
+  }
+
   findOneBySocketId(socketId: ISocketId): IPlayer | null {
     return this._players.get(socketId) || null;
   }
 
   findOneBySocketIdOrFail(socketId: ISocketId) {
-    const room = this.findOneBySocketId(socketId);
-    if (!room) {
+    const player = this.findOneBySocketId(socketId);
+    if (!player) {
       throw new NotFoundException('Player not found');
     }
 
-    return room;
+    return player;
   }
 
   create(socketId: ISocketId, name: string) {
     const player: IPlayer = {
       uuid: ulid() as IPlayerUuid,
+      roomUuid: null,
+      socketId,
       name,
     };
 

36eca9ac4d8e14e86d0ec39ea32502472f008833
Alexander <lembdev@gmail.com>
2025-10-19 13:22:26 +0200

    feat: use one handler to handle all WS messages

--- src/api/web-socket/gateways/game.gateway.ts
+++ src/api/web-socket/gateways/game.gateway.ts
@@ -1,51 +1,63 @@
 import type { Server, Socket } from 'socket.io';
-import type { ICommand } from 'src/contracts/commands';
-import { BadRequestException, Logger } from '@nestjs/common';
+import { BadRequestException, Logger, OnModuleInit } from '@nestjs/common';
 import {
-  ConnectedSocket,
-  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
-  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { CommandHandlerService } from 'src/app';
-import { Command } from 'src/contracts/commands';
+import { CommandHandlerService, PushHandlerService } from 'src/app';
+import { Command } from 'src/contracts/ws-commands';
 
 @WebSocketGateway({ cors: { origin: '*' } })
-export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class GameGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnModuleInit
+{
   private readonly _logger = new Logger(this.constructor.name);
 
   @WebSocketServer()
   private readonly _server: Server;
 
-  constructor(private readonly _commandHandlerService: CommandHandlerService) {}
+  constructor(
+    private readonly _commandHandlerService: CommandHandlerService,
+    private readonly _pushHandlerService: PushHandlerService,
+  ) {}
 
-  handleConnection(socketClient: Socket) {
-    this._logger.log(`Client connected: ${socketClient.id}`);
+  onModuleInit() {
+    this._pushHandlerService.setSocketServer(this._server);
   }
 
-  handleDisconnect(socketClient: Socket) {
-    this._logger.log(`Client disconnected: ${socketClient.id}`);
+  handleConnection(socket: Socket) {
+    this._logger.log(`Client connected: ${socket.id}`);
+
+    socket.onAny((event: string, ...args: any[]) => {
+      try {
+        const payload = args[0];
+        if (!payload) return;
+
+        this.messageHandler(socket, payload);
+      } catch (err) {
+        this._logger.error(`Error handling event '${event}': ${err}`);
+      }
+    });
   }
 
-  @SubscribeMessage('command')
-  commandMessageHandler(
-    @ConnectedSocket() socket: Socket,
-    @MessageBody() command: ICommand,
-  ): void {
+  handleDisconnect(socket: Socket) {
+    this._logger.log(`Client disconnected: ${socket.id}`);
+  }
+
+  messageHandler(socket: Socket, command: unknown) {
     const parseResult = Command.safeParse(command);
 
-    if (!parseResult.success) {
-      const error = new BadRequestException(parseResult.error);
-      this._logger.error(parseResult.error);
+    return parseResult.success
+      ? this._commandHandlerService.handle(socket, parseResult.data)
+      : this._emitError(socket, command, parseResult.error);
+  }
 
-      socket.emit('error', { error, command });
+  private _emitError(socket: Socket, command: unknown, errorDescr: unknown) {
+    const error = new BadRequestException(errorDescr);
+    this._logger.error(error);
 
-      return;
-    }
-
-    this._commandHandlerService.handleCommand(socket, parseResult.data);
+    socket.emit('error', { error, command });
   }
 }
